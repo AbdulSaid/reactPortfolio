2f753edb016131527c213a79d51b2b4269461fe8
Abdul Said <abdulrahman.asaid@gmail.com>
2022-02-08 22:50:04 -0500

    Finish Intro Page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,43 @@
 import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
-  return <div className="intro" id="intro"></div>;
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: [
+        "Full Stack Developer",
+        "Web3 Developer",
+        "Content Creator",
+        "Cloud Engineer",
+        "DevOps Engineer",
+      ],
+    });
+  }, []);
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/me.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Abdulrahman Said</h1>
+          <h3>
+            I am a <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
