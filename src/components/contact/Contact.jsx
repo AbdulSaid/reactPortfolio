837706ae02b645b3ea79b89f3cf9a907022df51d
Abdul Said <abdulrahman.asaid@gmail.com>
2022-02-10 00:50:41 -0500

    Change contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,33 @@
-import { useState } from "react";
+import { useContext, useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { ThemeContext } from "../../context";
 import "./contact.scss";
 
 export default function Contact() {
+  const formRef = useRef();
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
   const [message, setMessage] = useState(false);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    setMessage(true);
+    emailjs
+      .sendForm(
+        "service_x6jsaah",
+        "template_hpqpnio",
+        formRef.current,
+        "user_cNsDnIXW4XPjForsL5W1r"
+      )
+      .then(
+        (result) => {
+          setMessage(true);
+          console.log(result.text);
+          console.log("set done to true");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
   return (
     <div className="contact" id="contact">
