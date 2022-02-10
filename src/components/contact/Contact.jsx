1f26eaedcbbaaf18901add7840d7c394e5d6fb98
Abdul Said <abdulrahman.asaid@gmail.com>
2022-02-10 01:50:20 -0500

    Fix contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,7 @@
 import { useContext, useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import { ThemeContext } from "../../context";
+
 import "./contact.scss";
 
 export default function Contact() {
@@ -13,10 +14,10 @@ export default function Contact() {
     e.preventDefault();
     emailjs
       .sendForm(
-        "service_x6jsaah",
-        "template_hpqpnio",
+        process.env.SERVICE_ID,
+        process.env.TEMPLATE_ID,
         formRef.current,
-        "user_cNsDnIXW4XPjForsL5W1r"
+        process.env.USER_ID
       )
       .then(
         (result) => {
@@ -29,19 +30,61 @@ export default function Contact() {
         }
       );
   };
+  // background-color: #5d92e3;
   return (
-    <div className="contact" id="contact">
-      <div className="left">
-        <img src="assets/shake.svg" alt="" />
-      </div>
-      <div className="right">
-        <h2>Contact.</h2>
-        <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Email" />
-          <textarea placeholder="Message" />
-          <button type="submit">Send</button>
-          {message && <span>Thanks, I will reply ASAP :)</span>}
-        </form>
+    <div className="c">
+      <div className="c-wrapper">
+        <div className="c-left">
+          <h1 className="c-title">Let's discuss your project.</h1>
+          <div className="c-info">
+            <div className="c-info-item">
+              <img src="assets/phone.png" alt="" className="c-icon" />{" "}
+              416-262-4439
+            </div>
+            <div className="c-info-item">
+              <img src="assets/email.png" alt="" className="c-icon" />{" "}
+              abdulrahman.asaid@gmail.com
+            </div>
+            <div className="c-info-item">
+              <img src="assets/address.png" alt="" className="c-icon" />{" "}
+              Toronto, On
+            </div>
+          </div>
+          <div className="c-img">
+            <img src="assets/shake.svg" alt="" className="c-img-pic" />
+          </div>
+        </div>
+        <div className="c-right">
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <input
+              style={{ backgroundColor: darkMode && "#333" }}
+              type="text"
+              placeholder="Name"
+              name="user_name"
+            />
+            <input
+              style={{ backgroundColor: darkMode && "#333" }}
+              type="text"
+              placeholder="Subject"
+              name="user_subject"
+            />
+            <input
+              style={{ backgroundColor: darkMode && "#333" }}
+              type="text"
+              placeholder="Email"
+              name="user_email"
+            />
+            <textarea
+              style={{ backgroundColor: darkMode && "#333" }}
+              rows="5"
+              placeholder="Message"
+              name="message"
+            />
+            <button>Submit</button>
+            {message &&
+              "Thank you for your message. I will get back to you as soon as possible"}
+          </form>
+        </div>
       </div>
     </div>
   );
